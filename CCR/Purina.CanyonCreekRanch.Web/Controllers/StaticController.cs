7c887e16e2789482264cd910fe1b6b64b2abe4ea
Zack <zack@gigonaut.com>
2012-04-16 20:46:34 -0500

    static controller for statix

---
+++ CCR/Purina.CanyonCreekRanch.Web/Controllers/StaticController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Purina.CanyonCreekRanch.Web.Controllers
+{
+    public class StaticController : Controller
+    {
+        //
+        // GET: /Static/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
