728c52205dc0cf5092b757be717c12ef5c4e69b3
Fred Merlo <fredmerlo@gmail.com>
2012-04-21 22:25:48 -0500

    Added initial blog entities, models, views and controllers

---
+++ CCR/Purina.CanyonCreekRanch.Admin/Controllers/BlogEntryController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Purina.CanyonCreekRanch.Admin.Models;
+using Purina.CanyonCreekRanch.Common.Entities;
+
+namespace Purina.CanyonCreekRanch.Admin.Controllers
+{ 
+    public class BlogEntryController : Controller
+    {
+        private CCRDb db = new CCRDb();
+
+        public ViewResult Index()
+        {
+            return View(db.BlogEntries.ToList());
+        }
+
+        public ViewResult Details(int id)
+        {
+            BlogEntryModel blogentrymodel = db.BlogEntryModels.Find(id);
+            return View(blogentrymodel);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+      [HttpPost]
+        public ActionResult Create(BlogEntryModel blogentrymodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BlogEntryModels.Add(blogentrymodel);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(blogentrymodel);
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            BlogEntryModel blogentrymodel = db.BlogEntryModels.Find(id);
+            return View(blogentrymodel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(BlogEntryModel blogentrymodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogentrymodel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blogentrymodel);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            BlogEntryModel blogentrymodel = db.BlogEntryModels.Find(id);
+            return View(blogentrymodel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            BlogEntryModel blogentrymodel = db.BlogEntryModels.Find(id);
+            db.BlogEntryModels.Remove(blogentrymodel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
