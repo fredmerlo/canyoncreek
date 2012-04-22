da7d0d89809b1c784193489644bd14bbdf6fe733
Fred Merlo <fredmerlo@gmail.com>
2012-04-22 17:54:23 -0500

    added inital category products route, controller and partial view

---
+++ CCR/Purina.CanyonCreekRanch.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Purina.CanyonCreekRanch.Common.Entities;
+
+namespace Purina.CanyonCreekRanch.Web.Controllers
+{
+  public class CategoryController : Controller
+  {
+    private CCRDb db = new CCRDb();
+
+    public ActionResult Products(string url)
+    {
+      var category = db.Categories.FirstOrDefault<Category>(c => c.FriendlyUrl == url);
+      var products = db.Products.Where<Product>(p => p.ProductCategory.Id == category.Id).ToList();
+      
+      return PartialView(products);
+    }
+
+  }
+}
