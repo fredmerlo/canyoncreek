01ff30e1494d57cb78007290e5af2e642a688b03
Fred Merlo <fredmerlo@gmail.com>
2012-05-01 00:00:32 -0500

    wired up remaining static pages, added friendly urls to main category pages

---
+++ CCR/Purina.CanyonCreekRanch.Common/Helpers/TransferResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Purina.CanyonCreekRanch.Common.Helpers
+{
+  public class TransferResult : RedirectResult
+  {
+    public TransferResult(string url) : base(url) { }
+
+    public TransferResult(object routeValues)
+      : base(GetRouteURL(routeValues)) { }
+
+    private static string GetRouteURL(object routeValues)
+    {
+        UrlHelper url = new UrlHelper(new RequestContext(new HttpContextWrapper(HttpContext.Current), new RouteData()), RouteTable.Routes);
+        return url.RouteUrl(routeValues);
+    }
+
+    public override void ExecuteResult(ControllerContext context)
+    {
+      if (context == null)
+        throw new ArgumentNullException("context");
+
+      if (context.Controller.TempData != null && context.Controller.TempData.Count > 0)
+        throw new ApplicationException("TempData won't work with Server.TransferRequest!");
+
+      if (HttpRuntime.UsingIntegratedPipeline)
+      {
+        HttpContext.Current.Server.TransferRequest(Url, true);
+      }
+      else
+      {
+        HttpContext.Current.RewritePath(Url, false);
+        IHttpHandler httpHandler = new MvcHttpHandler();
+
+        httpHandler.ProcessRequest(HttpContext.Current);
+      }
+
+    }
+  }
+}
--- CCR/Purina.CanyonCreekRanch.Web/Controllers/HomeController.cs
+++ CCR/Purina.CanyonCreekRanch.Web/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Purina.CanyonCreekRanch.Common.Helpers;
+
 namespace Purina.CanyonCreekRanch.Web.Controllers
 {
   public class HomeController : Controller
@@ -20,16 +22,41 @@ namespace Purina.CanyonCreekRanch.Web.Controllers
       return View();
     }
 
+    [ActionName("natural-food-for-cats")]
+    public ActionResult NaturalFoodForCats() { return new TransferResult(new { controller = "Category", action = "Info", type = "cat" }); }
+
+    [ActionName("natural-food-for-dogs")]
+    public ActionResult NaturalFoodForDogs() { return new TransferResult(new { controller = "Category", action = "Info", type = "dog" }); }
+
+    [ActionName("snacks")]
+    public ActionResult Snacks() { return new TransferResult(new { controller = "Category", action = "Info", type = "snack" }); }
+
     [ActionName("holistic-living")]
-    public ActionResult HolisticLiving() { return View(); }
+    public ActionResult HolisticLiving() { return View("HolisticLiving"); }
 
     [ActionName("ingredient-dictionary")]
-    public ActionResult IngredientDictionary() { return View(); }
+    public ActionResult IngredientDictionary() { return View("IngredientDictionary"); }
 
     [ActionName("our-story")]
-    public ActionResult OurStory() { return View(); }
+    public ActionResult OurStory() { return View("OurStory"); }
 
     [ActionName("our-story-faq")]
-    public ActionResult OurStoryFaq() { return View(); }
+    public ActionResult OurStoryFaq() { return View("OurStoryFaq"); }
+
+    [ActionName("health-and-nutrition")]
+    public ActionResult HealthAndNutrition() { return View("HealthAndNutrition"); }
+
+    [ActionName("find-a-retailer")]
+    public ActionResult FindRetailser() { return View("FindRetailer"); }
+
+    [ActionName("site-map")]
+    public ActionResult SiteMap() { return View("SiteMap"); }
+
+    [ActionName("terms-and-conditions")]
+    public ActionResult TermsAndConditions() { return View("TermsAndConditions"); }
+
+    public ActionResult Contact() { return View(); }
+
+
   }
 }
