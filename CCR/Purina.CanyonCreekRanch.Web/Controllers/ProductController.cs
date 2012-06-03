11356cb7b0225d184a49f188383b8e56890871e7
Fred Merlo <fredmerlo@gmail.com>
2012-06-03 13:17:42 -0500

    Added left/right arrow navigation to product detail page

--- CCR/Purina.CanyonCreekRanch.Web/Controllers/ProductController.cs
+++ CCR/Purina.CanyonCreekRanch.Web/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,12 +15,18 @@ namespace Purina.CanyonCreekRanch.Web.Controllers
 
     public ActionResult Detail(string url)
     {
+      
       var product = db.Products.Where(p => p.Active == true && p.FriendlyUrl == url).FirstOrDefault();
 
-      if(product == null)
+      if (product == null)
         return RedirectToAction("Index", "Home");
 
+      var products = db.Products.Where(p => p.Active == true && p.ProductCategory.Id == product.ProductCategory.Id).OrderBy(o => o.Title).ToList();
       var detailType = Enum.GetName(typeof(Category.Classifier), product.ProductCategory.Type) + "Detail";
+      var index = products.IndexOf(product);
+
+      TempData.Add("previous", products[index == 0 ? products.Count - 1 : index - 1]);
+      TempData.Add("next", products[index + 1 == products.Count ? 0 : index + 1]);
 
       return View(detailType, product);
     }
